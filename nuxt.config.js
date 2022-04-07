20d35e8467b42cbe34b146544ada2f0ed4094111
Josef Kolář <thejoeejoee@gmail.com>
2022-04-07 22:35:52 +0200

    Tweaked map

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,8 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nabrnodobry.cz',
+    title: 'NA Brno dobrý 2022',
     htmlAttrs: {
       lang: 'en'
     },
@@ -19,45 +17,35 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/main.css',
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     '@nuxtjs/dotenv',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     ['nuxt-leaflet', { /* module options */ }],
 
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
 
   router: {
     base: process.env.NUXT_ROUTER_BASE_URL || '/'
   },
-
 }
--- pages/map.vue
+++ pages/map.vue
@@ -1,16 +1,24 @@
 <template>
-  <article>
+  <article class="flex flex-row">
+    <FancyLine class="w-8 border-b-0 bg-repeat-y h-full"
+               style="background-size: 70% auto;"
+               :style="{backgroundImage: `url(${require('~/assets/svg/line1-vertical.svg')})`}"/>
     <client-only>
       <l-map
         :zoom=13
         :center="[49.1996122, 16.6101164]"
       >
-        <l-tile-layer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+<!--        <l-tile-layer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>-->
 <!--        <l-tile-layer url="https://mapserver.mapy.cz/turist-m/{z}-{x}-{y}"></l-tile-layer>-->
+<!--        http://stamen-tiles-a.a.ssl.fastly.net/toner-lite/12/657/1591@2x.png-->
+        <l-tile-layer url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png"></l-tile-layer>
 
 <!--        <l-marker :lat-lng="[55.9464418, 8.1277591]"></l-marker>-->
       </l-map>
     </client-only>
+    <FancyLine class="w-8 border-b-0 bg-repeat-y h-full"
+               style="background-size: 70% auto;"
+               :style="{backgroundImage: `url(${require('~/assets/svg/line1-vertical.svg')})`}"/>
   </article>
 </template>
 
