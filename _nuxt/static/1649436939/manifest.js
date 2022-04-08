7a2ea799ffc232c50773fa22ba0901d3ca9d20b7
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-04-08 16:55:40 +0000

    deploy: 57c958eb3fd9fb7b389a504600d8ecc74291fcd6

---
+++ _nuxt/static/1649436939/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fsponsors","\u002Fabout","\u002F","\u002Ffaq","\u002Fschedule","\u002Fmap","\u002Fteam"]})
---
+++ _nuxt/static/1649436939/schedule/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnabrnodobry.cz\u002F_nuxt\u002Fstatic\u002F1649436939",layout:"default",error:a,serverRendered:true,routePath:"\u002Fschedule",config:{_app:{basePath:"\u002Fnabrnodobry.cz\u002F",assetsPath:"\u002Fnabrnodobry.cz\u002F_nuxt\u002F",cdnURL:a}}}}(null));
