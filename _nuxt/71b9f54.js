a7d3670a50691c580519529330f9bddd6eed50ad
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-04-08 17:06:26 +0000

    deploy: 63e70f41d67c40200884496f17ea183bf25df61e

--- _nuxt/71b9f54.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{367:function(n,e,t){"use strict";t.r(e);var o={name:"SponsorsPage"},r=t(17),component=Object(r.a)(o,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("article",[n._v("\n  sponsors\n")])}),[],!1,null,"554866cb",null);e.default=component.exports}}]);
--- _nuxt/static/1649437396/schedule/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/schedule", {data:[{events:[["Registration","4 PM - 7 PM"],["Accommodation entrance","from 5 PM"],["First workshop I Dunno the Name yet","8 PM"]]}],fetch:{},mutations:void 0});
