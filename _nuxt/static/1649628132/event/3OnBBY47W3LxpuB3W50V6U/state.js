c6ecfe8af81d9a4aad9c71df5dcb449df55265f0
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-04-10 22:02:15 +0000

    deploy: 209a8b289384057ef4a2ad0262257229703d0584

---
+++ _nuxt/static/1649628132/event/3OnBBY47W3LxpuB3W50V6U/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnabrnodobry.cz\u002F_nuxt\u002Fstatic\u002F1649628132",layout:"default",error:a,serverRendered:true,routePath:"\u002Fevent\u002F3OnBBY47W3LxpuB3W50V6U",config:{_app:{basePath:"\u002Fnabrnodobry.cz\u002F",assetsPath:"\u002Fnabrnodobry.cz\u002F_nuxt\u002F",cdnURL:a}}}}(null));
