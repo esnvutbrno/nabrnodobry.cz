3e7cb59d257892ff140168372c4c93cf9b921355
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-04-11 12:36:55 +0000

    deploy: 2b1c7df87e3458b214cc74c066d9b05ae85568f4

--- _nuxt/static/1649679888/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fabout","\u002F","\u002Ffaq","\u002Fcontact","\u002Fevent\u002F3OnBBY47W3LxpuB3W50V6U","\u002Fmap","\u002Fevent\u002F4wOICXMECkN9luFgpB8Iwv","\u002Fevent\u002F3ipxkOksn7zB6yoV7Qr3uh","\u002Fschedule","\u002Fevent\u002F28PbQZSMDnHEEM3IlpGi2t","\u002Fteam"]})
