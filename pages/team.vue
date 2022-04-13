432f54bc37343e9ba2b81a3baef6aa161106ff9a
Josef Kolář <thejoeejoee@gmail.com>
2022-04-13 11:40:52 +0200

    Schedule styling & icons & data

--- pages/team.vue
+++ pages/team.vue
@@ -21,28 +21,32 @@
 
     <div class="border-secondary border-b-4 my-8"></div>
 
-    <ul class="flex flex-row flex-wrap justify-center">
-      <li v-for="member in members" class="
-          p-4 font-bold flex flex-col items-center
+    <div v-for="(members, team) in teams">
+      <h2>{{ team }}</h2>
+      <ul class="flex flex-row flex-wrap justify-center">
+        <li v-for="member in members" class="
+          p-4 font-bold flex flex-col items-center justify-between
           w-1/3 sm:w-1/5 md:w-1/6
       ">
-        <ContentfulImage
-          :asset="member.fields.photo"
-          :default="require('~/assets/img/woman-icon.png')"
-          class="
-            w-full rounded-full mx-4
-            transition-transform duration-300 ease-in-out hover:scale-110
-          "
-        />
-        <h2 class="text-primary dark:text-white text-center my-1">{{ member.fields.name }}</h2>
-        <h3 class="text-xs text-gray-600 dark:text-gray-400 text-center">{{ member.fields.position }}</h3>
-      </li>
-    </ul>
+          <ContentfulImage
+            :asset="member.fields.photo"
+            :default="require('~/assets/img/woman-icon.png')"
+            class="
+              w-full rounded-full mx-4
+              transition-transform duration-300 ease-in-out hover:scale-110
+            "
+          />
+          <h2 class="text-primary dark:text-white text-center my-1">{{ member.fields.name }}</h2>
+          <h3 class="text-xs text-gray-600 dark:text-gray-400 text-center">{{ member.fields.position }}</h3>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import {createClient} from '~/plugins/contentful.js';
+import _ from 'lodash'
 
 export default {
   name: 'TeamPage',
@@ -60,7 +64,10 @@ export default {
       order: 'fields.order'
     });
 
-    return {heads: heads.items, members: members.items}
+    return {
+      heads: heads.items,
+      teams: _.groupBy(members.items, 'fields.team'),
+    }
   },
   head() {
     return {
