774ebf3e70f1d25dfc984b9df3d593a532007a15
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-04-20 20:33:15 +0000

    deploy: 3d7e99928d973d7c84aed1da248006d3507d7edf

--- _nuxt/static/1650486664/team/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Fnabrnodobry.cz\u002F_nuxt\u002Fstatic\u002F1650486664",layout:"default",error:a,serverRendered:b,routePath:"\u002Fteam",config:{_app:{basePath:"\u002Fnabrnodobry.cz\u002F",assetsPath:"\u002Fnabrnodobry.cz\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
