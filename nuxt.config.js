78a4ef6fe4847ef0378ad45b2a54f708917bf2b0
Josef Kolář <thejoeejoee@gmail.com>
2022-04-25 10:55:28 +0200

    Fixed axios baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,7 @@ export default {
 
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.NUXT_ROUTER_BASE_URL ? undefined : '/',
+    baseURL: process.env.FULL_BASE_URL || '/',
     prefix: process.env.NUXT_ROUTER_BASE_URL || '/',
   },
 
