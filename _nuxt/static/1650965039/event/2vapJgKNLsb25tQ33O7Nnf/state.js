6b2f38f3cb496a21dbab27dac47318af39ca02c6
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-04-26 09:24:10 +0000

    deploy: 2e7080f76b4c774ed32248fd74f50681b1951938

---
+++ _nuxt/static/1650965039/event/2vapJgKNLsb25tQ33O7Nnf/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650965039",layout:"default",error:a,serverRendered:b,routePath:"\u002Fevent\u002F2vapJgKNLsb25tQ33O7Nnf",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
---
+++ _nuxt/static/1650965039/event/3ZpAIO8PRUPYqyWapby57x/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650965039",layout:"default",error:a,serverRendered:b,routePath:"\u002Fevent\u002F3ZpAIO8PRUPYqyWapby57x",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
