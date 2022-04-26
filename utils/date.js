59b8135fa6633ef1203f658b17206cdeb150da8c
Josef Kolář <thejoeejoee@gmail.com>
2022-04-26 11:33:14 +0200

    Fixed locale defaults II

--- utils/date.js
+++ utils/date.js
@@ -2,6 +2,8 @@ const {DateTime, Duration, Settings} = require("luxon");
 
 Settings.defaultLocale = "en-US";
 
-DateTime.defaultZone = 'Europe/Prague';
+Settings.defaultZone = 'Europe/Prague';
+
+// DateTime.defaultZone = 'Europe/Prague';
 
 export {DateTime, Duration, Settings};
