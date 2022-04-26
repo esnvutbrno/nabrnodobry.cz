1abd217a2f833f2caf972e75f7250021312c384a
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-04-26 21:42:46 +0000

    deploy: 84f67bbb93177300e0a01b7a1f4140a054c294b0

--- _nuxt/static/1651003586/event/AJiS2rUjHDQLQx56uEHyC/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651003586",layout:a,error:b,state:{theme:{current:a}},serverRendered:c,routePath:"\u002Fevent\u002FAJiS2rUjHDQLQx56uEHyC",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:b}},colorMode:{preference:d,value:d,unknown:c,forced:false}}}("default",null,true,"system"));
