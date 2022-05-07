61c228c6b98ddb28287872c6e7e914597e4ea43f
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-05-07 07:47:02 +0000

    deploy: 152cd4de9ceaa5e5bff16e4c11a3232f39fefe55

--- _nuxt/static/1651884743/faq/food/2bxepb1cXvSVd77e6GgxYU/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/faq/food/2bxepb1cXvSVd77e6GgxYU", {data:[{}],fetch:{"data-v-76e2343d:0":{},"data-v-40edf253:0":{assets:[]},"data-v-bb1a04f2:0":{time:new Date(1651884747780)}},mutations:[["questions\u002FsetActiveCategorySlug","food"],["questions\u002FsetActiveQuestionId","2bxepb1cXvSVd77e6GgxYU"]]});
--- _nuxt/static/1651884743/faq/general/3cwXQXjzp3gMzAh3xW2Mlp/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/faq/general/3cwXQXjzp3gMzAh3xW2Mlp", {data:[{}],fetch:{"data-v-76e2343d:0":{},"data-v-40edf253:0":{assets:[]},"data-v-bb1a04f2:0":{time:new Date(1651884748970)}},mutations:[["questions\u002FsetActiveCategorySlug","general"],["questions\u002FsetActiveQuestionId","3cwXQXjzp3gMzAh3xW2Mlp"]]});
