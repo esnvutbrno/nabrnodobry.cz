764ecfcfce8eb74825eb4f73a8a0cdb96d86fc28
Josef Kolář <thejoeejoee@gmail.com>
2022-05-07 17:10:30 +0200

    Tweaked 404

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="min-h-[40vh] flex flex-col justify-center items-center gap-8 px-8">
-    <h1 class="font-esn text-primary font-bold font-4xl md:text-5xl text-center">
+    <span class="font-bold text-4xl md:text-8xl text-secondary">
+      😞
+    </span>
+    <h1 class="font-esn text-primary dark:text-white font-bold font-4xl md:text-5xl text-center">
       {{ error.message }}
     </h1>
-    <span class="font-mono text-xs text-gray-500">
+    <span class="font-mono text-xs text-gray-500 dark:text-gray-200">
         {{ $route.fullPath }}
     </span>
     <NuxtLink
-      class="text-center text-xl hover:underline border-primary
-      border-2 uppercase p-2 rounded font-medium text-primary"
+      class="
+        text-center text-xl hover:underline border-current text-secondary
+        border-2 uppercase p-2 rounded font-medium
+      "
       to="/"
-    >back to homepage
+    >
+      back to homepage
     </NuxtLink>
+    <span class="text-lg text-gray-500">
+      P.S. If you think this page should exist, please
+      <a
+        class="underline hover:text-gray-700 dark:hover:text-gray-300"
+        href="https://t.me/thejoeejoee"
+        target="_blank"
+      >contact us</a>.
+    </span>
   </div>
 </template>
 
