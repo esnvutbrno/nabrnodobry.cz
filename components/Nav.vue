cf2643f1afec832067f908d5e079abed9eebe50c
Josef Kolář <thejoeejoee@gmail.com>
2022-05-07 14:29:14 +0200

    Nav styles

--- components/Nav.vue
+++ components/Nav.vue
@@ -6,18 +6,15 @@
       dark:bg-primary dark:text-white
       z-[1111] select-none
     ">
-    <ul class="
-      flex flex-col md:flex-row w-1/3 md:w-full
-      md:space-x-2 justify-end
-    ">
-      <li class="flex items-center justify-end">
-        <nuxt-link class="nuxt-link" :to="{name: 'schedule'}">Schedule</nuxt-link>
+    <ul>
+      <li>
+        <nuxt-link :to="{name: 'schedule'}">Schedule</nuxt-link>
       </li>
-      <li class="flex items-center justify-end">
-        <nuxt-link class="nuxt-link whitespace-nowrap" :to="{name: 'about'}">About NA</nuxt-link>
+      <li>
+        <nuxt-link :to="{name: 'about'}">About NA</nuxt-link>
       </li>
-      <li class="flex items-center justify-end">
-        <nuxt-link class="nuxt-link tracking-tighter whitespace-nowrap"
+      <li>
+        <nuxt-link class="tracking-tighter whitespace-nowrap"
                    :to="{name: 'faq-category', params: {category: 'general'}}"
                    :class="{'nuxt-link-active': $route.name && $route.name.startsWith('faq-')}"
         >Q & A
@@ -30,15 +27,16 @@
     <nuxt-link
       class="
         flex flex-col sm:flex-row justify-center items-center
-        sm:mx-2 md:mx-4 my-2 md:my-0 px-2
+        my-2 sm:mx-2 md:mx-4 md:my-0
+        px-4
         hover:bg-gray-50 dark:hover:bg-primary
       "
       :to="{name: 'index'}"
       :class="{'hidden md:block md:invisible': $route.path === '/'}"
     >
       <svg
-          class="w-10 sm:w-12 fill-primary dark:fill-white dark:hover:fill-secondary"
-          viewBox="0 0 82.225755 72.054314"
+        class="w-10 sm:w-12 fill-primary dark:fill-white dark:hover:fill-secondary"
+        viewBox="0 0 82.225755 72.054314"
       >
         <use :xlink:href="require('@/assets/svg/cb.svg') + '#main'"/>
       </svg>
@@ -46,21 +44,18 @@
         NA Brno <br>dobrý
       </div>
     </nuxt-link>
-    <ul class="
-      flex flex-col md:flex-row w-1/3 md:w-full
-      md:space-x-2 justify-start
-    ">
-      <li class="flex items-center justify-start">
-        <nuxt-link class="nuxt-link" :to="{name: 'map'}">Map</nuxt-link>
+    <ul>
+      <li>
+        <nuxt-link :to="{name: 'map'}">Map</nuxt-link>
       </li>
-      <li class="flex items-center justify-start">
-        <nuxt-link class="nuxt-link" :to="{name: 'team'}">Team</nuxt-link>
+      <li>
+        <nuxt-link :to="{name: 'team'}">Team</nuxt-link>
       </li>
-      <li class="flex items-center justify-start">
-        <nuxt-link :to="{name: 'game'}" class="nuxt-link">Game</nuxt-link>
+      <li>
+        <nuxt-link :to="{name: 'game'}">Game</nuxt-link>
       </li>
-      <li class="flex items-center justify-start">
-        <nuxt-link class="nuxt-link" :to="{name: 'contact'}">Contact</nuxt-link>
+      <li>
+        <nuxt-link :to="{name: 'contact'}">Contact</nuxt-link>
       </li>
     </ul>
   </nav>
@@ -73,12 +68,38 @@ export default {
 </script>
 
 <style scoped>
-.nuxt-link {
-  @apply
-  px-2 py-2 md:py-8 hover:bg-gray-50
-  dark:hover:bg-transparent dark:hover:text-secondary
-}
 .nuxt-link-active {
   @apply text-secondary;
 }
+
+nav ul {
+  @apply flex flex-col sm:flex-row sm:flex-wrap md:flex-nowrap w-1/3 md:w-full
+  md:space-x-2 justify-end;
+}
+
+nav ul:last-of-type {
+  @apply justify-start;
+}
+
+nav ul li {
+  @apply
+  flex items-center
+  w-full sm:w-1/2 md:w-auto
+  ;
+}
+
+nav ul li a {
+  @apply
+  p-1 sm:p-2 md:py-8 hover:bg-gray-50
+  dark:hover:bg-transparent dark:hover:text-secondary
+  whitespace-nowrap
+  inline-block w-full
+  text-right sm:text-center
+  ;
+}
+
+nav ul:last-of-type li a {
+  @apply text-left sm:text-center;
+}
+
 </style>
