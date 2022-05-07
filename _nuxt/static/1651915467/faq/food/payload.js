6272d101d01abd8c0cb868f827ca4ab09b0bd88a
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-05-07 09:30:52 +0000

    deploy: 16fbe457947988ca2729429864bab8e3ff17d725

--- _nuxt/static/1651915467/faq/food/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/faq/food", {data:[{}],fetch:{"data-v-7deaca84:0":{},"data-v-40edf253:0":{assets:[]},"data-v-bb1a04f2:0":{time:new Date(1651915471842)}},mutations:[["questions\u002FsetActiveCategorySlug","food"],["questions\u002FsetActiveQuestionId",void 0]]});
--- _nuxt/static/1651915467/faq/transport/6NGyrtUh2YX3L3tcYHGgJb/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/faq/transport/6NGyrtUh2YX3L3tcYHGgJb", {data:[{}],fetch:{"data-v-7deaca84:0":{},"data-v-40edf253:0":{assets:[]},"data-v-bb1a04f2:0":{time:new Date(1651915473613)}},mutations:[["questions\u002FsetActiveCategorySlug","transport"],["questions\u002FsetActiveQuestionId","6NGyrtUh2YX3L3tcYHGgJb"]]});
