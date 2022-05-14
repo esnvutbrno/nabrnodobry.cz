507d298b6292a238fa26c23db56e5335ac85e4ef
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-05-14 18:57:08 +0000

    deploy: 0b7290d8043c59b003ad1b5333559f4d594efb5f

---
+++ _nuxt/static/1652554608/faq/food/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/faq/food", {data:[{}],fetch:{"data-v-40edf253:0":{assets:[]},"data-v-67a4fbf8:0":{time:new Date(1652554614333)}},mutations:[["questions\u002FsetActiveCategorySlug","food"],["questions\u002FsetActiveQuestionId",void 0]]});
---
+++ _nuxt/static/1652554608/game/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/game", {data:[{}],fetch:{"data-v-40edf253:0":{assets:[]},"data-v-67a4fbf8:0":{time:new Date(1652554613992)}},mutations:[]});
