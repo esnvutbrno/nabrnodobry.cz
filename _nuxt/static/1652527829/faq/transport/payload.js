53cfd6730b0ae2685a36d02a485b65fb239f7257
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-05-14 11:30:50 +0000

    deploy: 0b7290d8043c59b003ad1b5333559f4d594efb5f

---
+++ _nuxt/static/1652527829/faq/transport/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/faq/transport", {data:[{}],fetch:{"data-v-40edf253:0":{assets:[]},"data-v-67a4fbf8:0":{time:new Date(1652527847112)}},mutations:[["questions\u002FsetActiveCategorySlug","transport"],["questions\u002FsetActiveQuestionId",void 0]]});
