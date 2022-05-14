adad18626b78f27ddbda298008f4a807fd7fe62d
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-05-14 11:52:09 +0000

    deploy: 0b7290d8043c59b003ad1b5333559f4d594efb5f

---
+++ _nuxt/static/1652529105/faq/general/1VJkxAmDIoOs7u0Jzm9IL7/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/faq/general/1VJkxAmDIoOs7u0Jzm9IL7", {data:[{}],fetch:{"data-v-40edf253:0":{assets:[]},"data-v-67a4fbf8:0":{time:new Date(1652529113421)}},mutations:[["questions\u002FsetActiveCategorySlug","general"],["questions\u002FsetActiveQuestionId","1VJkxAmDIoOs7u0Jzm9IL7"]]});
