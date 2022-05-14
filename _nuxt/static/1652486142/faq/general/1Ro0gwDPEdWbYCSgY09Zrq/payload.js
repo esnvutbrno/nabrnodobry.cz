f46f71b73298e2f4dd9d2e16deac89c584ca35d8
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-05-14 01:46:55 +0000

    deploy: 0b7290d8043c59b003ad1b5333559f4d594efb5f

--- _nuxt/static/1652486142/faq/general/1Ro0gwDPEdWbYCSgY09Zrq/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/faq/general/1Ro0gwDPEdWbYCSgY09Zrq", {data:[{}],fetch:{"data-v-40edf253:0":{assets:[]},"data-v-67a4fbf8:0":{time:new Date(1652486148867)}},mutations:[["questions\u002FsetActiveCategorySlug","general"],["questions\u002FsetActiveQuestionId","1Ro0gwDPEdWbYCSgY09Zrq"]]});
--- _nuxt/static/1652486142/faq/social-programme/4PAoUepKaInGoyWvGfz0RX/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/faq/social-programme/4PAoUepKaInGoyWvGfz0RX", {data:[{}],fetch:{"data-v-40edf253:0":{assets:[]},"data-v-67a4fbf8:0":{time:new Date(1652486153942)}},mutations:[["questions\u002FsetActiveCategorySlug","social-programme"],["questions\u002FsetActiveQuestionId","4PAoUepKaInGoyWvGfz0RX"]]});
