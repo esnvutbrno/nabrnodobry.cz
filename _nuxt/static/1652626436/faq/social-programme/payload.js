60c8f8cf470569e453dfe076704e5d334382a4cc
thejoeejoee <thejoeejoee@users.noreply.github.com>
2022-05-15 15:13:55 +0000

    deploy: 0b7290d8043c59b003ad1b5333559f4d594efb5f

--- _nuxt/static/1652626436/faq/social-programme/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/faq/social-programme", {data:[{}],fetch:{"data-v-40edf253:0":{assets:[]},"data-v-67a4fbf8:0":{time:new Date(1652626443021)}},mutations:[["questions\u002FsetActiveCategorySlug","social-programme"],["questions\u002FsetActiveQuestionId",void 0]]});
